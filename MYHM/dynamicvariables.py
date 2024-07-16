5af00f6d95bb30324a721849cd5b554d1846bdde
Mayhem Bill <5598969+MayhemBill@users.noreply.github.com>
2024-07-16 11:47:21 -0400

    Update dynamicvariables.py

--- MYHM/dynamicvariables.py
+++ MYHM/dynamicvariables.py
@@ -1,10 +1,22 @@
-def currentWeather():
-    data = requests.get('http://api.openweathermap.org/data/2.5/weather?appid=3e6826fb716810018759216195531995&units=imperial&q={city}').json()
-    temperature = round(data['main']['feels_like'])
-    return f"{temperature}°F"
-
 def customCurrentTime():
     return datetime.now().strftime('%H:%M %p')
 
-addDRPCValue("weather", currentWeather)
+def currentWeatherIcon():
+    icondata = requests.get(f"http://api.weatherapi.com/v1/forecast.json?key=8214f5a2223c42a084e233902240603&q=34221&days=1").json()
+    iconresponse = icondata['current']['condition']['icon']
+    return f"https:{iconresponse}"
+
+def currentWeatherTemp():
+    tempdata = requests.get(f"http://api.weatherapi.com/v1/forecast.json?key=8214f5a2223c42a084e233902240603&q=34221&days=1").json()
+    tempresponse = round(tempdata['current']['feelslike_f'])
+    return f"{tempresponse}°F"
+
+def currentWeatherHumidity():
+    humditydata = requests.get(f"http://api.weatherapi.com/v1/forecast.json?key=8214f5a2223c42a084e233902240603&q=34221&days=1").json()
+    humitidyresponse = humditydata['current']['humidity']
+    return f"{humitidyresponse}%"
+
 addDRPCValue("custom_time", customCurrentTime)
+addDRPCValue("weathericon", currentWeatherIcon)
+addDRPCValue("temperature", currentWeatherTemp)
+addDRPCValue("humidity", currentWeatherHumidity)
